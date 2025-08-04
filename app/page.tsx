6993d6d11bf5df8e795a9203b8875b79dde675be
James Donaghue <james.donaghue@gmail.com>
2025-08-04 15:00:42 -0500

    add link to search implementation on dd page

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,7 @@ export default function Home() {
       <div className="flex-col items-center justify-items-center ">
         <h1>A fast, rich and dynamic React Dropdown component</h1>
         <div><a className="text-lg" target="_blank" rel="noopener noreferrer" href="https://github.com/jdonaghue/dropdown">Source on Github</a></div>
+        <div><a className="text-lg" target="_blank" rel="noopener noreferrer" href="https://github.com/jdonaghue/dropdown/blob/main/packages/components/dropdown/search.ts">Search implementation on Github</a></div>
       </div>
       <div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center justify-items-center sm:items-start">
